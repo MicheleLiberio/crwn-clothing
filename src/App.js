c772cbf1ad15ed1a32d3935aad2d6be6ece569cd
Michele Liberio <michele.liberio@macnil.local>
2021-10-20 16:01:10 +0200

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,42 +9,28 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
-  
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot =>{
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          }, () => {
-            console.log(this.state);
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
-          
-          console.log(this.state);
-        })
+        });
       }
-      else {
-        this.setState({ currentUser: userAuth });
-      }
-    })
+
+      setCurrentUser(userAuth);
+    });
   }
 
   componentWillUnmount() {
@@ -53,15 +40,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route path='/shop' component={ShopPage} />
-            <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
--- src/firebase/test.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-
-const firestore = firebase.firestore();
-
-firestore.collection('users').doc('YRmVYmWEUmaH7yHeoey7').collection('cartItems').doc('mv5XHR72qo9wyNLZ6mw2')
-firestore.doc('/users/YRmVYmWEUmaH7yHeoey7/cartItems/mv5XHR72qo9wyNLZ6mw2');
-firestore.collection('/users/YRmVYmWEUmaH7yHeoey7/cartItems/mv5XHR72qo9wyNLZ6mw2');
